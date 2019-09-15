7895da2cc22de0722a1fd4c47ba986ba2ddf18f5
Emilio <a01651283@itesm.mx>
2019-09-15 18:42:13 -0500

    JSON modification

---
+++ grammys/js/grammys.js
@@ -0,0 +1,22 @@
+$.ajax({
+    url : 'https://raw.githubusercontent.com/lohemilio/DesarrolloAppsWeb/master/grammys/data/grammys.json',
+    type : 'GET',
+    dataType : 'json',
+    success : function(data) {
+        console.log(data);
+      /*let newHtml = ''
+  
+      for( let i = 0; i < data.length; i++ ) {
+        newHtml += `
+          <option value="${data[i].field_id}">
+            ${data[i].field}
+          </option>
+        `
+      }
+  
+      $('#category_types').append(newHtml)*/
+    },
+    error : function(errorMsg) {
+      console.log(errorMsg)
+    }
+  })
