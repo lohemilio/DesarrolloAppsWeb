6b7370d4e77e7f4bd080ec5e3278afb43c079331
Emilio <a01651283@itesm.mx>
2019-09-15 21:14:15 -0500

    Sections added with JSON data

--- grammys/js/grammys.js
+++ grammys/js/grammys.js
@@ -4,7 +4,7 @@ $.ajax({
     dataType : 'json',
     success : function(data) {
         console.log(data);
-      /*let newHtml = ''
+      let newHtml = ''
   
       for( let i = 0; i < data.length; i++ ) {
         newHtml += `
@@ -13,10 +13,58 @@ $.ajax({
           </option>
         `
       }
-  
-      $('#category_types').append(newHtml)*/
+      $('#category_types').append(newHtml)
+      loadCategoriesJSON()
     },
     error : function(errorMsg) {
       console.log(errorMsg)
     }
   })
+
+  function loadCategoriesJSON() {
+    $.ajax({
+      url : 'https://raw.githubusercontent.com/lohemilio/DesarrolloAppsWeb/master/grammys/data/grammys.json',
+      type : 'GET',
+      dataType : 'json',
+      success : function(data) {
+        let newHtml = ''
+        $('#category_types').on('change', function(event) {
+          let id = $(this).val()
+  
+          for( let i = 0; i < data.length; i++ ) {
+            if ( id == data[i].field_id ) {
+              $('#field').text(data[i].field)
+              $('#description').text(data[i].description)
+              for( let j = 0; j < data[i].categories.length; j++ ) {
+                newHtml += `
+                  <h3>
+                    ${data[i].categories[j].category_name}
+                  </h3>
+                `
+
+                for(let k = 0;k < data[i].categories[j].nominees.length;k++){
+
+                    newHtml+=`
+                    <ul>
+                        <li><span>
+                        ${data[i].categories[j].nominees[k].nominee}
+                        </span></li>
+                        <p>${data[i].categories[j].nominees[k].artist}</p>
+                        <p>${data[i].categories[j].nominees[k].info}</p>
+                    </ul>
+                    `
+                }
+              }
+              $('#nominees_section').append(newHtml)
+
+              
+            }
+          }
+        })
+      },
+      error : function(errorMsg) {
+        console.log(errorMsg)
+      }
+    })
+  }
+  
