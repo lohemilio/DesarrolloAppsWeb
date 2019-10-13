0d6a2462bc5da9a102022319c08cd973cba4e3c6
Emilio <a01651283@itesm.mx>
2019-10-13 18:55:46 -0500

    last changes

--- node/weather-app-2.0/app.js
+++ node/weather-app-2.0/app.js
@@ -6,66 +6,75 @@ var temperature
 var precipProbability 
 var city
 
-
-
-const getCity = function(place,callback){
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + place + '.json?access_token=' + credentials.MAPBOX_TOKEN
-
-    request({url, json: true},function(error,response){
-
-        if (error){
-            callback(error,undefined)
-        } else {
-            const data = response.body
-
-            if(data.Response == 'False'){
-                callback(data.Error,undefined)
-        } else {
-            const info = {
-                latitude: data.features[0].center[1], longitude: data.features[0].center[0]
-        
-            }
-            
-            callback(undefined, info)
-        }
-    }
-        
-        
-    })
-}
-
-
 const getWeatherConditions = function(latitude,longitude,callback){
     const url = 'https://api.darksky.net/forecast/' + credentials.DARK_SKY_SECRET_KEY + '/' + latitude + ',' + longitude + '?exclude=[minutely,hourly,daily,alerts,flags]&units=si'
 
     request({url, json: true},function(error,response){
         if (error){
             callback('Unable to connect to the weather service', undefined)
-        } else{
-            const data = response.body
-            if(data.Response == 'False'){
-                callback(data.Error,undefined)
-            } else{
-                const info = {
-                    summary: data.currently.summary,
-                    temperature: data.currently.temperature,
-                    precipProbability: (response.body.currently.precipProbability) * 100
-                }
-                console.log(summary + '. Actually the temperature is ' + temperature + '°C. There is ' + precipProbability + '% of precipitation probability.')
-                callback(undefined,info)
-            }
+        } else if(response.body["error"]){
+            callback(response.body["error"],undefined)
+        } 
+        
+        else{
+            const data = response.body.currently
+             
+            const msg = data.summary + '. Actually the temperature is ' + String(data.temperature) + '°C. There is ' + String(data.precipProbability) + '% of precipitation probability.'
+                
+            callback(error,msg)
 
         }
     })
 }
 
+const getCoordinates = function(place,callback){
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + place + '.json?access_token=' + credentials.MAPBOX_TOKEN
+
+    request({url, json: true},function(error,response){
+
+        if (error){
+            callback(error,undefined)
+        } 
+        else if(response.body.message){
+            callback(response.body.message, undefined)
+        }
+        else if(!response.body.features[0]){
+            callback("city not found", undefined)
+        }
+        else {
+            const data = response.body
+
+             
+            const latitude = data.features[0].center[1]
+            const longitude = data.features[0].center[0]
+            callback(undefined, latitude, longitude)
+        }
+    
+        
+        
+    })
+}
 
 
-module.exports = {
-    getCity : getCity,
-    getWeatherConditions : getWeatherConditions
-  }
 
+
+
+
+getCoordinates("Monterrey", function(error, latitude, longitude){
+    if(error){
+      console.log(error)
+    }
+    else{
+      getWeatherConditions(latitude, longitude, function(error2, output){
+        if(error2){
+          console.log(error2)
+        }
+        else{
+          console.log(output)
+        }
+      })
+    }
+  })
         
             
 
